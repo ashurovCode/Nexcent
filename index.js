41c9a2adcb5e11e5073924ffc13ade33d3c1bead
ashurovCode <isfandiyora49@gmail.com>
2025-10-28 11:16:45 +0700

    added smoothe scrol animation and developed authorization pages

--- index.js
+++ index.js
@@ -49,3 +49,16 @@ activeLink.forEach((link) => {
     this.classList.add('active')
   })
 })
+
+const faders = document.querySelectorAll('.fade-in')
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible')
+      observer.unobserve(entry.target)
+    }
+  })
+})
+
+faders.forEach((fader) => observer.observe(fader))
